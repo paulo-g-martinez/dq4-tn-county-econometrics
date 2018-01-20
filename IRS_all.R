fd67a25ea4e81c70cba12f0ec42343f01b36bb52
massih <massih@NashvilleRealtyGroup.local>
2018-01-20 13:51:20 -0600

    merging ed data

--- IRS_all.R
+++ IRS_all.R
@@ -4,7 +4,9 @@ library("tidyverse")
 library("magrittr")
 library("purrr", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
 library("readxl", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
+library("gdata", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
 
+options(stringsAsFactors=F)
 # list of spreadsheets
 irs_xls <-  list.files(path=".", pattern = "??zp43tn.xls")
 
@@ -13,42 +15,70 @@ for (i in irs_xls) {
   df_name <-  paste("df_irs_", substr(i, 1, 2), sep="")
   print(paste(df_name," is:",i))
   # Creating dataframes 
-  assign(df_name, read_excel(i, range = cell_rows(4:4725)))
+  assign(df_name, read.xls(i,col_names = F))
 }
 
 df_list = list(df_irs_11, df_irs_12, df_irs_13, df_irs_14, df_irs_15)
 
 create_header <- function(df_x) {
     # A1. Fill the blank column with the same header left to it
-    for (i in 1:length(df_x)) {
-      top_row_3 <- substr(colnames(df_x)[i], 1, 3)
-      if (top_row_3 == "X__") { #blank / merged header
-        colnames(df_x)[i] <- colnames(df_x)[i-1]  
+    df_cols <- c(df_x[4,])
+    df_cols2 <- c(df_x[5,])
+  
+    for (i in 1:length(df_cols)) {
+      if (i == 1) {
+        c1 <- df_cols[i]
       }
+      print(df_cols[i])
+      if (df_cols[i] != "") {
+        c1 <- df_cols[i]
+      }
+      
+      full_name <- paste(c1, df_cols2[i])
+      # B1. everything lowercase
+      full_name <- str_to_lower(full_name)
+      
+      # B2. Remove space from beginning and end
+      full_name <- str_trim(full_name) 
+      
+      # B3. Replace space with underscore
+      full_name <- gsub(" +|-","_",full_name)
+      
+      # B4. Remove brackets and parantheses
+      full_name <- gsub("\\[.*\\]|\\n|\\(.*\\)|'", "",full_name)
+      df_x[5,i] <- full_name
+      
+      # if (is.na(df_x[3,i]) == T) { 
+      #   df_x[i] <- df_x[i + 1]  
+      # }
     }
-    # A2. Merge rows 4 & 5 and put it in row 5. 
+    colnames(df_x) <- df_x[5,]
+    df_x <- df_x[-c(1:5),]
+    df_x <- df_x[, !names(df_x) %in% c("na_na")]
+    df_x
     
-    for (i in 1:length(df_x)) { # This IF block prevents NA values be merged 
-      if (is.na(df_x[1,i]) == TRUE) {
-        df_x[1,i] <- colnames(df_x)[i]
-      } else {
-        df_x[1,i] <- paste(colnames(df_x)[i] , df_x[1,i])
-      }
-    }
-    # A: Row 5 will have the unique title
+
+    # 
+    # # A2. Merge rows 4 & 5 and put it in row 5. 
+    # 
+    # for (i in 1:length(df_x)) { # This IF block prevents NA values be merged 
+    #   df_x[2,i] <- paste(df_x[1,i] , df_x[2,i])
+    #   }
+    # 
+    # # A: Row 5 will have the unique title
     
     # B: Standardize column headers
     # B1. everything lowercase
-    df_x[1,] <- tolower(df_x[1,])
-    
-    # B2. Remove space from beginning and end
-    df_x[1,] <- str_trim(df_x[1,]) 
-    
-    # B3. Replace space with underscore
-    df_x[1,] <- gsub(" +|-","_",df_x[1,])
-    
-    # B4. Remove brackets and parantheses
-    df_x[1,] <- gsub("\\[.*\\]|\\n|\\(.*\\)|'", "",df_x[1,])
+    # df_x[2,] <- tolower(df_x[1,])
+    # 
+    # # B2. Remove space from beginning and end
+    # df_x[2,] <- str_trim(df_x[1,]) 
+    # 
+    # # B3. Replace space with underscore
+    # df_x[2,] <- gsub(" +|-","_",df_x[1,])
+    # 
+    # # B4. Remove brackets and parantheses
+    # df_x[2,] <- gsub("\\[.*\\]|\\n|\\(.*\\)|'", "",df_x[1,])
 }
 
 # Run the function for all dataframes
--- Xander.R
+++ Xander.R
@@ -1,19 +1,18 @@
 # XANDER's github
 
-setwd("/Users/xandermorrison/git/data-question-4-data-question-4-supreme-watch")
+setwd("/Users/ghfmhf/git/data-question-4-data-question-4-supreme-watch")
 
-install.packages("gdata")
 library("gdata")
 library("tidyverse")
 
 options(stringsAsFactors = FALSE)
 
-files <- list.files(path="./tax_data")
+files <- list.files(path="./data/tax_data")
 
 for (f in files) {
   v = paste("df", substr(f, 1, 2), sep="")
   print(v)
-  d = paste("./tax_data", f, sep="/")
+  d = paste("./data/tax_data", f, sep="/")
   assign(v, read.xls(d, dec = ".", blank.lines.skip = T))
 }
 
@@ -60,7 +59,43 @@ dfs <- lapply(dfs, column_assigner)
 dfs <- lapply(dfs, clean_zips)
 
 ach_profile <- read_csv("data/achievement_profile_data_with_CORE.csv")
+zip_cleaned <- zip_code()
 
 
-zip_code <- read.xls("data/zip_code_database.xlsx", dec=".", blank.lines.skip = TRUE)
-zip_code <- zip_code[(zip_code["state"] == "TN"),]
+# The ZIP code script is commented to avoid running every time slowing down the procedure
+# zip_code <- read.xls("data/zip_code_database.xlsx", dec=".", blank.lines.skip = TRUE)
+# zip_code <- zip_code[(zip_code["state"] == "TN"),]
+
+
+membership <- read_csv("data/data_2015_membership_school.csv") %>% 
+  filter(race_or_ethnicity == "All Race/Ethnic Groups", gender == "All Genders")
+
+crosswalk <- read_xls("data/data_district_to_county_crosswalk.xls")
+
+# Joining two databases (District number in Crosswalk is tha same as system in school data)
+merged_ed <- merge(ach_profile, crosswalk, by.y = "District Number", by.x = "system", all.x = T)
+
+df_irs_11 <- dfs[[1]] %>% 
+  group_by(zipcode) %>% 
+  summarise(num_ret_tot=sum(number_of_returns, na.rm = T), avg_agi=mean(adjusted_gross_income, na.rm = T))
+
+irs_ed_merge <- merge(df_irs_11,zip_code, by.x = "zipcode", by.y = "zip", all.x = T) %>% 
+  drop(              
+       [2] "num_ret_tot"                  
+       [3] "avg_agi"                      
+       [4] "type"                         
+       [5] "decommissioned"               
+       [6] "primary_city"                 
+       [7] "acceptable_cities"            
+       [8] "unacceptable_cities"          
+       [9] "state"                        
+       [10] "county"                       
+       [11] "timezone"                     
+       [12] "area_codes"                   
+       [13] "world_region"                 
+       [14] "country"                      
+       [15] "latitude"                     
+       [16] "longitude"                    
+       [17] "irs_estimated_population_2014")
+
+
